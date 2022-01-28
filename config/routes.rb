38c8c2d04687465f67e36641460d6e8f9c768986
Sudip Das <sudip5428@gmail.com>
2022-01-28 18:46:08 +0530

    create friend app and basic card operations

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :friends
+  get 'home/about'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+ root 'home#index'
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
